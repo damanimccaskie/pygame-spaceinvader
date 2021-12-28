ea0cda62a347122ee29ab6c544c5545693aedc0e
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2021-12-28 18:55:56 -0400

    Secondary Commit with Player and Enemy added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 # Initialize the pygame
@@ -11,13 +13,64 @@ pygame.display.set_caption("Space Invader")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load("player.png")
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load("enemy.png")
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
 # Game Loop
 running = True
 while running:
+
+    # Change screen color RGB - Red, Green, Blue 0-255
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Change screen color RGB - Red, Green, Blue 0-255
-    screen.fill((255, 0, 0))
+        # If keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+                print("Left arrow is pressed")
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+                print("Right arrow is pressed")
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("Keystroke has been released")
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    # Check for edge detection for the player
+    if playerX < 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # Call player function
+    player(playerX, playerY)
+
+    # Call enemy function
+    enemy(enemyX, enemyY)
+    
     pygame.display.update()
